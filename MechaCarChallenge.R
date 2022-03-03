b45a178fc0a7a6f129d150944fa3c3ab2abb7941
Martha Richardson <mgsrichard@yahoo.com>
2022-03-03 10:23:07 -0500

    deliverable 1 programmed

---
+++ MechaCarChallenge.R
@@ -0,0 +1,15 @@
+
+#import dependencies
+library("tidyverse")
+
+
+#import MechaCar data file
+MechaCar_mpg <- read.csv(file ='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
+
+#Perform multiple linear regression
+Mecha_Multiple_Regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD, data = MechaCar_mpg)
+print(Mecha_Multiple_Regression)
+
+#Use summary() to get p value and r squared value for linear regression model
+Mecha_summary <- summary(Mecha_Multiple_Regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD, data = MechaCar_mpg))
+print(Mecha_summary)
