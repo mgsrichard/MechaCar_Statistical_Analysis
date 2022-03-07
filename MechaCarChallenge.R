e418bced5e1ab644722487c0e8c9acc931bd3d32
Martha Richardson <mgsrichard@yahoo.com>
2022-03-07 09:50:23 -0500

    added ground clearance

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,9 +7,9 @@ library("tidyverse")
 MechaCar_mpg <- read.csv(file ='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
 
 #Perform multiple linear regression
-Mecha_Multiple_Regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD, data = MechaCar_mpg)
+Mecha_Multiple_Regression <- lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = MechaCar_mpg)
 print(Mecha_Multiple_Regression)
 
 #Use summary() to get p value and r squared value for linear regression model
-Mecha_summary <- summary(Mecha_Multiple_Regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD, data = MechaCar_mpg))
+Mecha_summary <- summary(Mecha_Multiple_Regression <- lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = MechaCar_mpg))
 print(Mecha_summary)
