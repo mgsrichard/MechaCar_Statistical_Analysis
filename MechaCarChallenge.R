d77ec735257278bc466c52f0b41d084e6387489b
Martha Richardson <mgsrichard@yahoo.com>
2022-03-07 14:31:41 -0500

    deliv 3 and small change on 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -29,3 +29,27 @@ print(Suspension_total_summary)
 #Create a summary by lot of mean, median, variance, standard deviation for the PSI column in Suspension Coil data
 Suspension_lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
 print(Suspension_lot_summary)
+
+#Deliverable 3 - T-tests on Suspension Coils
+
+# t-test 1 - Is the PSI across all lots, i.e. in total, statistically different from the population mean of 1,500 pounds per square inch?
+#use the one sample t-test since we are testing it against a presumed population mean
+
+t.test(Suspension_Coil$PSI, mu=1500)
+
+#t-test 2 Is the Lot 1 PSI statistically different from the presumed population mean of 1500 psi?
+#use the one sample t-tesy since we are testing against a presumed population mean
+
+Suspension_Coil_Lot1 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot1')
+t.test(Suspension_Coil_Lot1$PSI, mu=1500)
+
+#t-test 3 Is the Lot 2 PSI statistically different from the presumed population mean of 1500 psi?
+#use the one sample t-tesy since we are testing against a presumed population mean
+
+Suspension_Coil_Lot2 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot2')
+t.test(Suspension_Coil_Lot2$PSI, mu=1500)
+
+#t-test 2 Is the Lot 3 PSI statistically different from the presumed population mean of 1500 psi?
+#use the one sample t-tesy since we are testing against a presumed population mean
+Suspension_Coil_Lot3 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot3')
+t.test(Suspension_Coil_Lot3$PSI, mu=1500)
